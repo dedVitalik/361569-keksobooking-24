8615a6c892a5e8b97eb19da4b20deff9337da91d
Dedvitalik <vitaliy.vinnik@gmail.com>
2021-10-14 23:29:00 +0300

    markup generator

--- js/main.js
+++ js/main.js
@@ -1,6 +1,1 @@
-import {createAnnouncement} from './modules/datagen.js';
-import {constants} from './modules/constants.js';
-
-const similarAnnouncements = Array.from({length: constants.SIMILAR_ANNOUNCEMENT_COUNT}, createAnnouncement);
-// eslint-disable-next-line no-console
-console.log(similarAnnouncements);
+import './modules/markupgen.js';
